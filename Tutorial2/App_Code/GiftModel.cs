e1454fe00af5ad80ea5a3ccc7af0c47b46cbfced
James Kessler <james.kessler@gmail.com>
2012-04-12 12:19:09 -0600

    done tutorial 2

---
+++ Tutorial2/App_Code/GiftModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for GiftModel
+/// </summary>
+public class GiftModel
+{
+    public string Title { get; set; }
+    public double Price { get; set; }
+}
